12399bdbed098807e7daaff1473709a2e78dac86
VADemon <wademon@yandex.ru>
2015-09-02 18:18:53 +0200

    Updated examples for UniMenu

--- unimenu_menus.lua
+++ unimenu_menus.lua
@@ -93,11 +93,21 @@ do	-- to eliminate the copy-paste job, we'll use locals so we can edit the menuI
 		end
 	end
 	
+	-- here we have the option execute weaponName and weaponPrice directly, turning the values into returned strings
+	-- because these functions rely on the data table which we're not planning to change during gameplay
+	-- You only should do this optimization if your server is lagging because of UniMenu (and that's very unlikely)
+	-- in this case, call the functions with arguments (0, <YOUR DATA TABLE>)
+	-- For example:
+	--
+	--	local tempData = {weapon = "AK-47", price = 2500, id = 30}
+	--	unimenu_addButton(menuID, weaponName(0, tempData), weaponPrice(0, tempdata), tempData, onClick)
+	--
 	unimenu_addButton(menuID, weaponName, weaponPrice, {weapon = "AK-47", price = 2500, id = 30}, onClick)
 	unimenu_addButton(menuID, weaponName, weaponPrice, {weapon = "AWP", price = 3000, id = 35}, onClick)
 	unimenu_addButton(menuID, weaponName, weaponPrice, {weapon = "M4A1", price = 2600, id = 32}, onClick)
 	unimenu_addButton(menuID, weaponName, weaponPrice, {weapon = "Five-Seven", price = 500, id = 6}, onClick)
 	
+	--
 	
 	local messageColorName = function (id, data)
 		return data.colorName .. " colored message"
@@ -116,11 +126,31 @@ do	-- to eliminate the copy-paste job, we'll use locals so we can edit the menuI
 		end
 	end
 	
+	-- same here, the messageColorName and messageColorPrice will stay the same all game long, so we COULD turn them into strings as well
 	unimenu_addButton(menuID, messageColorName, messageColorPrice, {colorName = "Red", colorcode = "255000000", price = 50}, messageOnClick)
 	unimenu_addButton(menuID, messageColorName, messageColorPrice, {colorName = "Green", colorcode = "000255000", price = 30}, messageOnClick)
 	unimenu_addButton(menuID, messageColorName, messageColorPrice, {colorName = "Blue", colorcode = "000000255", price = 20}, messageOnClick)
 	unimenu_addButton(menuID, messageColorName, messageColorPrice, {colorName = "White", colorcode = "255255255", price = 250}, messageOnClick)
 	unimenu_addButton(menuID, messageColorName, messageColorPrice, {colorName = "Black", colorcode = "000000000", price = 100}, messageOnClick)
+	
+	--
+	
+	local randomMessageOnClick = function (id)
+		local r, g, b = tostring(math.random(0, 255)), tostring(math.random(0, 255)), tostring(math.random(0, 255))
+		local color = "©" .. string.rep("0", 3 - #r) .. r .. string.rep("0", 3 - #g) .. g .. string.rep("0", 3 - #b) .. b
+		local price = math.random(30, 250)
+
+		if player(id,"money") >= price then
+			msg(color .."Hello, this is a randomly colored message!")
+			parse("setmoney "..id.." "..player(id,"money") - price)
+		else
+			msg2(id,"You don't have enough money to send a randomly colored message!")
+		end
+	end
+	
+	unimenu_addButton(menuID, "Randomly colored message", "30-250$", {}, randomMessageOnClick)
+	
+	--
 	
 	local setMoney = function (id, data)
 		parse("setmoney ".. id .." ".. data.money)
@@ -128,6 +158,21 @@ do	-- to eliminate the copy-paste job, we'll use locals so we can edit the menuI
 	
 	unimenu_addButton(menuID, "Set money", "$1337", {money = 1337}, setMoney)
 	unimenu_addButton(menuID, "Set money", "$9000", {money = 9000}, setMoney)
+	
+	--
+	
+	local playersOnline = function ()
+		return #player(0, "table") .. " out of ".. game("sv_maxplayers")
+	end
+	
+	local playersOnlineOnClick = function ()
+		local online = playersOnline
+		return "There are " .. online() .. " players on the server!"
+	end
+	
+	-- unlike in the examples above, playersOnline function may change it's return value at any time during the gameplay.
+	-- that's why we'll leave it a function and let UniMenu call it when the menu is calculated
+	unimenu_addButton(menuID, "Players online:", playersOnline, {}, playersOnlineOnClick)
 end
 
 -- add more of those  * do ... end * blocks if you need more menus.
